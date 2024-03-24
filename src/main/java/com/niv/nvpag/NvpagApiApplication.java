69fd822ac831b34d758b69c0120427c916aeb088
kevinfjq <kevifjq@gmail.com>
2024-03-24 00:16:42 -0300

    initial commit

---
+++ src/main/java/com/niv/nvpag/NvpagApiApplication.java
@@ -0,0 +1,13 @@
+package com.niv.nvpag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvpagApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvpagApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niv/nvpag/NvpagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niv.nvpag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvpagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
