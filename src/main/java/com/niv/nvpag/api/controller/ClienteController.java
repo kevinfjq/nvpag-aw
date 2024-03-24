ba8ee10c02f94544e4eeef5ba8d675d0de1b5823
kevinfjq <kevifjq@gmail.com>
2024-03-24 18:28:24 -0300

    feat: add crud endpoints

--- src/main/java/com/niv/nvpag/api/controller/ClienteController.java
+++ src/main/java/com/niv/nvpag/api/controller/ClienteController.java
@@ -1,17 +1,56 @@
 package com.niv.nvpag.api.controller;
 
 import com.niv.nvpag.domain.model.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.niv.nvpag.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
-    @GetMapping("/clientes")
-    public List<Cliente> list() {
+    //    @Autowired
+    private final ClienteRepository clienteRepository;
 
-        return Arrays.asList(new Cliente(), new Cliente());
+    @GetMapping
+    public List<Cliente> list() {
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> findCliente(@PathVariable Long clienteId) {
+        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+        if(cliente.isPresent()) return ResponseEntity.ok(cliente.get());
+
+        return ResponseEntity.notFound().build();
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Cliente create(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> update(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+        if(!clienteRepository.existsById(clienteId)) return ResponseEntity.notFound().build();
+
+       cliente.setId(clienteId);
+       cliente = clienteRepository.save(cliente);
+
+       return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("{clienteId}")
+    public ResponseEntity<Void> delete(@PathVariable Long clienteId) {
+        if(!clienteRepository.existsById(clienteId)) return ResponseEntity.notFound().build();
+        clienteRepository.deleteById(clienteId);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/niv/nvpag/domain/model/Cliente.java
+++ src/main/java/com/niv/nvpag/domain/model/Cliente.java
@@ -1,17 +1,23 @@
 package com.niv.nvpag.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 }
